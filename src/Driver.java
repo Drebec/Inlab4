c4a763d9b6bace9ed65cd84e793eb80bfff65312
Drebec <drew.beck33@gmail.com>
2015-02-11 02:30:38 -0600

    Added additional data types

--- src/Driver.java
+++ src/Driver.java
@@ -14,15 +14,17 @@ public class Driver {
         Stack stack1 = new Stack();
         Stack stack2 = new Stack();
 
-        stack1.push(0);
-        stack1.push(5);
-        stack1.push(2);
+        stack1.push("Drew");
+        stack1.push("Austin");
+        stack1.push("Megan");
         stack1.push(8);
         stack1.push(-4);
+        stack1.push(4.56);
+        stack1.push('A');
 
-        stack2.push(1);
-        stack2.push(3);
-        stack2.push(7);
+        stack2.push("Sean");
+        stack2.push("Watson");
+        stack2.push("Joe");
         stack2.push(-3);
         stack2.push(12);
         
